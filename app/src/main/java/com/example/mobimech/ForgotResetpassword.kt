0806725406caa88b50f1fe6b1c71ac24a41baa71
SydNs <sydneynsiimbe@gmail.com>
2021-03-19 23:26:29 -0400

    added new edited images with a less brightness to add the overlay

--- app/src/main/java/com/example/mobimech/ForgotResetpassword.kt
+++ app/src/main/java/com/example/mobimech/ForgotResetpassword.kt
@@ -1,11 +1,27 @@
 package com.example.mobimech
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobimech.databinding.ActivityForgotResetpasswordBinding
 
 class ForgotResetpassword : AppCompatActivity() {
+
+    lateinit var activityForgotResetpasswordBinding: ActivityForgotResetpasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_resetpassword)
+        val forgotResetpasswordBindingview=activityForgotResetpasswordBinding.root
+        setContentView(forgotResetpasswordBindingview)
+
+        activityForgotResetpasswordBinding.registerlinkresetpass.setOnClickListener {
+            startActivity(Intent(this,Registration::class.java))
+            finish()
+        }
+        activityForgotResetpasswordBinding.loginlinkresetpass.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
     }
 }
