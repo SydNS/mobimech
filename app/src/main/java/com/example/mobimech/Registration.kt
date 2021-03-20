33892e2ac56fa1d54eac55662fb88f14d789d211
SydNs <sydneynsiimbe@gmail.com>
2021-03-19 22:25:20 -0400

    using viewbinding and intent for  this activity to link to the rest

--- app/src/main/java/com/example/mobimech/Registration.kt
+++ app/src/main/java/com/example/mobimech/Registration.kt
@@ -1,11 +1,26 @@
 package com.example.mobimech
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.mobimech.databinding.ActivityRegistrationBinding
 
 class Registration : AppCompatActivity() {
+
+    lateinit var activityRegistrationBinding:ActivityRegistrationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+
+        val activityRegistrationBinding=ActivityRegistrationBinding.inflate(layoutInflater)
+        val registrationview:View=activityRegistrationBinding.root
+        setContentView(registrationview)
+        activityRegistrationBinding.loginlink.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+
+
     }
 }
