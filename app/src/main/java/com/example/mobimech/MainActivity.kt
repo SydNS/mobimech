2b434bb8de011f9be45c12a171810010cb7956a2
SydNs <sydneynsiimbe@gmail.com>
2021-03-20 00:00:53 -0400

    realigned the code and added more links

--- app/src/main/java/com/example/mobimech/MainActivity.kt
+++ app/src/main/java/com/example/mobimech/MainActivity.kt
@@ -9,19 +9,30 @@ import com.example.mobimech.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var activityMainBinding:ActivityMainBinding
+    lateinit var activityMainBinding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding= ActivityMainBinding.inflate(layoutInflater)
-        val mainactivityview:View=activityMainBinding.root
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val mainactivityview: View = activityMainBinding.root
         setContentView(mainactivityview)
 
-        activityMainBinding.loginbtn.setOnClickListener { Toast.makeText(this,"Dean Got it",Toast.LENGTH_LONG).show() }
+        activityMainBinding.loginbtn.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Dean Got it",
+                Toast.LENGTH_LONG
+            ).show()
+        }
 
         activityMainBinding.registerlink.setOnClickListener {
-            startActivity(Intent(this,Registration::class.java))
+            startActivity(Intent(this, Registration::class.java))
+            finish()
+
+        }
+        activityMainBinding .resetpasswordlink . setOnClickListener {
+            startActivity(Intent(this, ForgotResetpassword::class.java))
             finish()
 
         }
