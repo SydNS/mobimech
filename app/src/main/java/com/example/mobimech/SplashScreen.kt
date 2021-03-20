525e2010e5cd01595d4cfdc8f2a4506277891a64
SydNs <sydneynsiimbe@gmail.com>
2021-03-20 05:51:59 -0400

    abandoned this class since it had no bindingViewClass

--- app/src/main/java/com/example/mobimech/SplashScreen.kt
+++ app/src/main/java/com/example/mobimech/SplashScreen.kt
@@ -1,59 +1,38 @@
-package com.example.mobimech
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import com.example.mobimech.AuthDestinationFrags.LoginFrag
-import com.example.mobimech.databinding.FragmentLoginBinding
-import com.example.mobimech.databinding.FragmentSplashScreenBinding
-
-class SplashScreen : Fragment() {
-
-    lateinit var splashScreenBinding: FragmentSplashScreenBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        splashScreenBinding = FragmentSplashScreenBinding.inflate(inflater, container, false)
-        return splashScreenBinding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        Navigation.findNavController(view).navigate(R.id.action_splashScreen_to_loginFrag)
-
-
-
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SplashScreen().apply {
-
-            }
-    }
-}
-
-
+//package com.example.mobimech
+//
+//
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.view.animation.AnimationUtils
+//import android.widget.Toast
+//import androidx.fragment.app.Fragment
+//import androidx.navigation.Navigation
+//import com.example.mobimech.AuthDestinationFrags.LoginFrag
+//import com.example.mobimech.databinding.FragmentLoginBinding
+//import com.example.mobimech.databinding.FragmentSplashScreenBinding
+//
+//class SplashScreen : Fragment() {
+//
+//    lateinit var splashScreenBinding: FragmentSplashScreenBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        splashScreenBinding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+//        return splashScreenBinding.root
+//    }
+//
+//
+//}
+//
+//
