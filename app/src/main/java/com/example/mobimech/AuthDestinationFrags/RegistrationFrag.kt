fa8ac23e70c02aa551241c63eb624b2f3ca834d1
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-22 14:07:22 -0700

    added the ability to pick info fromthe inputtextlayout

--- app/src/main/java/com/example/mobimech/AuthDestinationFrags/RegistrationFrag.kt
+++ app/src/main/java/com/example/mobimech/AuthDestinationFrags/RegistrationFrag.kt
@@ -1,11 +1,17 @@
 package com.example.mobimech.AuthDestinationFrags
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.mobimech.R
+import com.example.mobimech.databinding.FragmentRegistrationBinding
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +27,9 @@ class RegistrationFrag : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var mAuth: FirebaseAuth? = null
+    lateinit var registrationBinding: FragmentRegistrationBinding
+    private lateinit var emailtext:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +44,37 @@ class RegistrationFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        registrationBinding= FragmentRegistrationBinding.inflate(inflater)
+        var view=registrationBinding.root
+        mAuth = FirebaseAuth.getInstance()
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        registrationBinding.registerbtn.setOnClickListener {
+            var emailtext= registrationBinding.emaillogin.editText?.text.toString().trim()
+            var usernametext= registrationBinding.usenname.editText?.text.toString().trim()
+            var passtext1= registrationBinding.passlogin.editText?.text.toString().trim()
+            var passtext2= registrationBinding.passlogin2.editText?.text.toString().trim()
+
+            Toast.makeText(activity,"$emailtext $usernametext $passtext1 $passtext2",Toast.LENGTH_SHORT).show()
+
+            if ( emailtext.isNotEmpty()|| usernametext.isNotEmpty()){
+                if(passtext1==passtext2 ) {
+                    Toast.makeText(activity, "$passtext1 is equal $passtext2", Toast.LENGTH_SHORT)
+                        .show()
+                    Navigation.findNavController(view)
+                        .navigate(R.id.action_registrationFrag_to_loginFrag)
+                }else{
+                    registrationBinding.passlogin.isErrorEnabled=true
+                    registrationBinding.passlogin2.isErrorEnabled=true
+
+                }
+            }
+        }
+
     }
 
     companion object {
