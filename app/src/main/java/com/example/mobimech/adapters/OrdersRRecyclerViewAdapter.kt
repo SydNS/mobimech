c9b1f08374da45e7bb75e2f395329bf727130779
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-22 19:42:19 -0700

    viewbinding works well and the getitemcount still linting

--- app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
@@ -8,15 +8,19 @@ import com.example.mobimech.R
 import com.example.mobimech.databinding.OrderlistLayoutBinding
 import com.example.mobimech.models.DisplayItem
 
-class OrdersRRecyclerViewAdapter(val orderlistItem:  ArrayList<OrderListItem>) : RecyclerView.Adapter<OrdersRRecyclerViewAdapter.ViewHolder>() {
+class OrdersRRecyclerViewAdapter(val orderlistItem: ArrayList<OrderListItem>) :
+    RecyclerView.Adapter<OrdersRRecyclerViewAdapter.ViewHolder>() {
 
     lateinit var orderlistLayoutBinding: OrderlistLayoutBinding
-    class ViewHolder(var viewBinding: OrderlistLayoutBinding) : RecyclerView.ViewHolder(viewBinding.root)
+
+    class ViewHolder(var viewBinding: OrderlistLayoutBinding) :
+        RecyclerView.ViewHolder(viewBinding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 //        val view=LayoutInflater.from(parent.context).inflate(R.layout.orderlist_layout, parent, false)
-        orderlistLayoutBinding= OrderlistLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        val view=orderlistLayoutBinding
+        orderlistLayoutBinding =
+            OrderlistLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = orderlistLayoutBinding
         return ViewHolder(view)
     }
 
@@ -25,7 +29,7 @@ class OrdersRRecyclerViewAdapter(val orderlistItem:  ArrayList<OrderListItem>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.viewBinding.mecahnname.text=orderlistItem[position].mechName
-        holder.viewBinding.orderdate.text=orderlistItem[position].OrderDate
+        holder.viewBinding.mecahnname.text = orderlistItem[position].mechName
+        holder.viewBinding.orderdate.text = orderlistItem[position].OrderDate
     }
 }
