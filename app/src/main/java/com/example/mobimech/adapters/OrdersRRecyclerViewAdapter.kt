00a7abc79c2c019c49689a8e595dae599bfd4d84
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-22 17:32:40 -0700

    implementing the list in he orders lists

--- app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.mobimech.adapters
 
-class OrdersRRecyclerViewAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobimech.models.DisplayItem
+
+class OrdersRRecyclerViewAdapter(val orderlistItem:  ArrayList<OrderListItem>) : RecyclerView.Adapter<OrdersRRecyclerViewAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
