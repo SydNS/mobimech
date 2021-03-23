b214ef1fba737a839225d9292d392a3a57393367
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-22 19:09:17 -0700

    using viewbinding actually lesses the code

--- app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
@@ -11,16 +11,14 @@ import com.example.mobimech.models.DisplayItem
 class OrdersRRecyclerViewAdapter(val orderlistItem:  ArrayList<OrderListItem>) : RecyclerView.Adapter<OrdersRRecyclerViewAdapter.ViewHolder>() {
 
     lateinit var orderlistLayoutBinding: OrderlistLayoutBinding
-    class ViewHolder(var viewBinding: OrderlistLayoutBinding) : RecyclerView.ViewHolder(itemView) {
-        val tvname=
-
+    class ViewHolder(var viewBinding: OrderlistLayoutBinding) : RecyclerView.ViewHolder(viewBinding.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 //        val view=LayoutInflater.from(parent.context).inflate(R.layout.orderlist_layout, parent, false)
         orderlistLayoutBinding= OrderlistLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        val view=orderlistLayoutBinding.root
+        val view=orderlistLayoutBinding
         return ViewHolder(view)
     }
 
@@ -29,6 +27,7 @@ class OrdersRRecyclerViewAdapter(val orderlistItem:  ArrayList<OrderListItem>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        holder.viewBinding.mecahnname.text=orderlistItem[position].mechName
+        holder.viewBinding.orderdate.text=orderlistItem[position].OrderDate
     }
 }
