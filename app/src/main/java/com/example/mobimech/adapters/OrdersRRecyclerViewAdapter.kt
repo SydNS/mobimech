3aaff2b5c411d4351d9638b2ea549412b35a7159
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-22 18:54:14 -0700

    made the recyler view adapter have viewbinding

--- app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/OrdersRRecyclerViewAdapter.kt
@@ -1,24 +1,34 @@
 package com.example.mobimech.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mobimech.R
+import com.example.mobimech.databinding.OrderlistLayoutBinding
 import com.example.mobimech.models.DisplayItem
 
 class OrdersRRecyclerViewAdapter(val orderlistItem:  ArrayList<OrderListItem>) : RecyclerView.Adapter<OrdersRRecyclerViewAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    lateinit var orderlistLayoutBinding: OrderlistLayoutBinding
+    class ViewHolder(var viewBinding: OrderlistLayoutBinding) : RecyclerView.ViewHolder(itemView) {
+        val tvname=
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+//        val view=LayoutInflater.from(parent.context).inflate(R.layout.orderlist_layout, parent, false)
+        orderlistLayoutBinding= OrderlistLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view=orderlistLayoutBinding.root
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        orderlistItem.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 }
