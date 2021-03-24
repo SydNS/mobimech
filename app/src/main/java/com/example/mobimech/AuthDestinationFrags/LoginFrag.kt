1d42f2346bfba1e94f530f669480c582b33127c0
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-24 09:52:11 -0700

    incoporated how to login a user

--- app/src/main/java/com/example/mobimech/AuthDestinationFrags/LoginFrag.kt
+++ app/src/main/java/com/example/mobimech/AuthDestinationFrags/LoginFrag.kt
@@ -1,6 +1,8 @@
 package com.example.mobimech.AuthDestinationFrags
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import com.example.mobimech.R
 import com.example.mobimech.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,12 +31,16 @@ class LoginFrag : Fragment() {
 
     lateinit var loginBinding: FragmentLoginBinding
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -49,15 +56,7 @@ class LoginFrag : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         loginBinding.loginbtn.setOnClickListener {
 //            Navigation.findNavController(view).navigate(R.id.action_loginFrag_to_homeFrag)
-            Navigation.findNavController(view).navigate(
-                R.id.action_loginFrag_to_homeFrag,
-                null,
-                NavOptions.Builder()
-                    .setPopUpTo(
-                        R.id.splash,
-                        true
-                    ).build()
-            )
+
 
             Toast.makeText(
                 activity,
@@ -70,7 +69,7 @@ class LoginFrag : Fragment() {
             Navigation.findNavController(view).navigate(R.id.action_loginFrag_to_registrationFrag)
             Toast.makeText(
                 activity,
-                "Register",
+                "Register for an account",
                 Toast.LENGTH_LONG
             ).show()
 
@@ -79,7 +78,7 @@ class LoginFrag : Fragment() {
         loginBinding.resetpasswordlink.setOnClickListener {
             Toast.makeText(
                 activity,
-                "Dean Got it",
+                "Reset Your Password",
                 Toast.LENGTH_LONG
             ).show()
             Navigation.findNavController(view).navigate(R.id.action_loginFrag_to_resetPassword)
@@ -87,6 +86,41 @@ class LoginFrag : Fragment() {
         }
 
 
+    }
+
+
+    private fun signIn(email: String, password: String,view: View) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+            if(it.isSuccessful){
+                Log.d(TAG, "createUserWithEmail:success")
+                val user = auth?.currentUser
+                Toast.makeText(activity,user.toString(),Toast.LENGTH_SHORT).show()
+
+//
+//                Navigation.findNavController(view)
+//                    .navigate(R.id.action_loginFrag_to_homeFrag)
+
+                Navigation.findNavController(view).navigate(
+                    R.id.action_loginFrag_to_homeFrag,
+                    null,
+                    NavOptions.Builder()
+                        .setPopUpTo(
+                            R.id.splash,
+                            true
+                        ).build()
+                )
+
+
+            }else{
+
+                Toast.makeText(activity,"Sorry failed to login ${it.exception}",Toast.LENGTH_SHORT).show()
+
+
+            }
+        }
+
+        // [END sign_in_with_email]
     }
 
     companion object {
