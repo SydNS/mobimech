6bd40e2ecc8331169408c791834dfa93ae0f4163
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-25 03:38:55 -0700

    make a fragment for making an order

---
+++ app/src/main/java/com/example/mobimech/homeui/MakeOrder.kt
@@ -0,0 +1,23 @@
+package com.example.mobimech.homeui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MakeOrder : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
