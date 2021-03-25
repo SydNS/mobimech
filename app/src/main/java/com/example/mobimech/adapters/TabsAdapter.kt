204ba9e31c06ac2288de2081c92c44964cc31ee5
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-25 03:22:06 -0700

    fragments to swipe through

--- app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
@@ -5,6 +5,8 @@ package com.example.mobimech.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.mobimech.homeui.LogsFrag
+import com.example.mobimech.homeui.MakeOrder
 
 class TabsAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
@@ -18,12 +20,20 @@ class TabsAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         when (position) {
             0 -> {
+                return MakeOrder()
 
             }
             1 -> {
+                return LogsFrag()
 
             }
         }
 
     }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return super.getPageTitle(position)
+
+    }
+
 }
