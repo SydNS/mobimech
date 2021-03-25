2ed24f05c110dd123565617f9e5a6f7769ba12df
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-25 03:03:21 -0700

    adding a when branch to cater for displaying the swiped tab page

--- app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
@@ -5,14 +5,25 @@ package com.example.mobimech.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.PagerAdapter
 
 class TabsAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    private val fragments: List<Fragment> = ArrayList()
+    private val fragmentTitle: List<String> = ArrayList()
+
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return 2
     }
 
     override fun getItem(position: Int): Fragment {
-        TODO("Not yet implemented")
+        when (position) {
+            0 -> {
+
+            }
+            1 -> {
+
+            }
+        }
+
     }
 }
