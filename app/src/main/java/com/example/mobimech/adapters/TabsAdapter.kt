69170c38e6ea8afa1e1fe27bf8147c42f5f71f2c
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-25 02:54:56 -0700

    implemented the need methods for the fragmentpageradapter

--- app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
@@ -1,3 +1,18 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.mobimech.adapters
 
-class TabsAdapter 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+
+class TabsAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
