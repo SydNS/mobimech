a74d1d633bdf4066eeeb18e4749fe3a01ac89078
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-25 05:05:55 -0700

    added the default returns for each of the methods in the adapter

--- app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
+++ app/src/main/java/com/example/mobimech/adapters/TabsAdapter.kt
@@ -2,6 +2,7 @@
 
 package com.example.mobimech.adapters
 
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -10,30 +11,26 @@ import com.example.mobimech.homeui.MakeOrder
 
 class TabsAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-    private val fragments: List<Fragment> = ArrayList()
-    private val fragmentTitle: List<String> = ArrayList()
-
     override fun getCount(): Int {
         return 2
     }
 
     override fun getItem(position: Int): Fragment {
         when (position) {
-            0 -> {
-                return MakeOrder()
-
-            }
-            1 -> {
-                return LogsFrag()
-
-            }
+            0 -> return MakeOrder()
+            1 -> return LogsFrag()
         }
 
+        return MakeOrder()
     }
 
+    @Nullable
     override fun getPageTitle(position: Int): CharSequence? {
-        return super.getPageTitle(position)
-
+        when (position) {
+            0 -> return "Make Order"
+            1 -> return "History"
+        }
+        return null
     }
 
 }
