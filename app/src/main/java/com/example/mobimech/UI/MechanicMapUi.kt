8ea6aec1e9e7fba55f42ec05e56feb25df0a6948
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-27 14:58:58 -0700

    created a mechanic activity seperate from users but with the same logic as the users and then change the url references to match mechanics

---
+++ app/src/main/java/com/example/mobimech/UI/MechanicMapUi.kt
@@ -0,0 +1,192 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.mobimech.UI
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.*
+import android.location.Location
+import android.location.LocationListener
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RelativeLayout
+import androidx.core.app.ActivityCompat
+import com.example.mobimech.R
+import com.firebase.geofire.GeoFire
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
+
+//Mechanic's Map Activity
+
+class MechanicMapUi : AppCompatActivity(), OnMapReadyCallback,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+    com.google.android.gms.location.LocationListener {
+
+    private lateinit var mMap: GoogleMap
+    lateinit var googleApiClient: GoogleApiClient
+    lateinit var location: Location
+    private lateinit var lastLocation: Location
+    lateinit var locationRequest: LocationRequest
+
+    //getting request customer's id
+    private var customerID = ""
+    private var customerPickupLocation: LatLng? = null
+
+    var driverLocationref: DatabaseReference? = null
+    private lateinit var firbasedatabase:FirebaseDatabase
+//    val geoQuery: GeoQuery? = null
+
+    private var currentLogOutCustomerStatus = false
+    var radius = 1.0
+    var driverFound = false
+    var requestType = false
+    var driver_found_id: String? = null
+    var MechanicRef: DatabaseReference? = null
+    var DriverMarker: Marker? = null
+    var PickUpMarker: Marker? = null
+    var DriverLocationRefListener: ValueEventListener? = null
+
+
+    //    private var logoutbtn: Button? = null
+//    private var profilePic: CircleImageView? = null
+    private var relativeLayout: RelativeLayout? = null
+    private var mAuth: FirebaseAuth? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_map_ui)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        firbasedatabase =FirebaseDatabase.getInstance("https://mobimech-d46d0-default-rtdb.firebaseio.com")
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        buildGoogleApiClient()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        mMap.isMyLocationEnabled=true
+
+        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onConnected(p0: Bundle?) {
+
+        locationRequest= LocationRequest()
+        locationRequest.interval=1000
+        locationRequest.fastestInterval=1000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+            googleApiClient,
+            locationRequest,
+            this
+        )
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLocationChanged(location: Location) {
+        lastLocation=location
+        val latlong = LatLng(location.latitude, location.longitude)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlong))
+        mMap.addMarker(
+            MarkerOptions().position(latlong)
+                .title("User Location")
+        )
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17F))
+
+//        here we record the mechanics location in the DB
+        val userID: String? = FirebaseAuth.getInstance().currentUser?.uid
+
+        val MechanicsAvailabilityRefInTheDb: DatabaseReference =firbasedatabase.reference.child("Mechanics Available")
+
+        val geoFireAvailability = GeoFire(MechanicsAvailabilityRefInTheDb)
+        val MechanicsWorkingRef: DatabaseReference =FirebaseDatabase.getInstance().reference.child("Mechanics Working")
+
+//        val geoFireWorking = GeoFire(MechanicsWorkingRef)
+
+
+
+
+
+    }
+
+    @Synchronized
+    protected fun buildGoogleApiClient() {
+        googleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+        googleApiClient.connect()
+    }
+}
