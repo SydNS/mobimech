8bceda27fb1b1170dd6d83d173e22eac81bee60d
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-11 23:43:59 -0700

    created this uui to cater for the problem with the setContentView in the initial setting ui activity

---
+++ app/src/main/java/com/example/mobimech/UI/Setting.kt
@@ -0,0 +1,12 @@
+package com.example.mobimech.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobimech.R
+
+class Setting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting2)
+    }
+}
