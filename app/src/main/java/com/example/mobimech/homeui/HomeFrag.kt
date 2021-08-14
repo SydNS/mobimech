369e5659d3ba884f8a061ed681aeea73995a4174
SydNs <sydneynsiimbe@gmail.com>
2021-08-15 00:52:11 +0300

    check if there is a user

--- app/src/main/java/com/example/mobimech/homeui/HomeFrag.kt
+++ app/src/main/java/com/example/mobimech/homeui/HomeFrag.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -110,6 +111,10 @@ class HomeFrag : Fragment() {
         fragmentHomeBinding.tabLayout.setupWithViewPager(fragmentHomeBinding.viewpager)
         fragmentHomeBinding.logoutbutton.setOnClickListener {
             auth?.signOut()
+            NavHostFragment
+                .findNavController(this)
+                .navigate(R.id.action_homeFrag_to_loginFrag)
+
 
         }
         return fragmentHomeBinding.root
