dc1d7c709d2a6dd75e33078ad86df0db271f3a93
SydNs <sydneynsiimbe@gmail.com>
2021-08-14 19:43:21 +0300

    created  a reset sceen for the user lost passwords

--- app/src/main/java/com/example/mobimech/AuthDestinationFrags/ResetPassword.kt
+++ app/src/main/java/com/example/mobimech/AuthDestinationFrags/ResetPassword.kt
@@ -1,13 +1,25 @@
 package com.example.mobimech.AuthDestinationFrags
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.navigation.Navigation
 import com.example.mobimech.R
+import com.example.mobimech.databinding.FragmentRegistrationBinding
 import com.example.mobimech.databinding.FragmentResetPasswordBinding
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.android.gms.tasks.OnCompleteListener
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +36,13 @@ class ResetPassword : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
+    private var mAuth: FirebaseAuth? = null
+    lateinit var registrationBinding: FragmentRegistrationBinding
+    private lateinit var emailtext: String
+    private var firebaseAuthListner: FirebaseAuth.AuthStateListener? = null
+    private lateinit var firbasedatabase: FirebaseDatabase
+
     lateinit var resetPasswordBinding: FragmentResetPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +51,8 @@ class ResetPassword : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     override fun onCreateView(
@@ -40,7 +61,7 @@ class ResetPassword : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        resetPasswordBinding= FragmentResetPasswordBinding.inflate(inflater)
+        resetPasswordBinding = FragmentResetPasswordBinding.inflate(inflater)
         return resetPasswordBinding.root
     }
 
@@ -48,17 +69,40 @@ class ResetPassword : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         resetPasswordBinding.btnresetpass.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_resetPassword_to_loginFrag)
+            var email: String = resetPasswordBinding.useremailreset.editText?.text.toString().trim()
+            resetPassword(registrationBinding.root, email)
         }
 
         resetPasswordBinding.loginlinkresetpass.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_resetPassword_to_loginFrag)
         }
         resetPasswordBinding.registerlinkresetpass.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_resetPassword_to_registrationFrag)
+            Navigation.findNavController(view)
+                .navigate(R.id.action_resetPassword_to_registrationFrag)
         }
     }
 
+    fun resetPassword(v: View, em: String) {
+
+        when {
+            TextUtils.isEmpty(em) -> {
+                Toast.makeText(activity, "Enter your email!", Toast.LENGTH_SHORT).show();
+            }
+
+            em.isValidEmail()-> Toast.makeText(activity, "Enter a Valid email!", Toast.LENGTH_SHORT).show();
+            else -> mAuth?.sendPasswordResetEmail(em)?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Navigation.findNavController(v).navigate(R.id.action_resetPassword_to_loginFrag)
+
+                    Log.d(TAG, "Email sent.")
+                }
+            }
+        }
+    }
+
+    private fun String.isValidEmail(): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
