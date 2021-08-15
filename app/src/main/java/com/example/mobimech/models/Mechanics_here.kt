2f3a3e2268cd2b24f0c6bbb1741361e7e97c6b7f
SydNs <sydneynsiimbe@gmail.com>
2021-08-15 05:05:40 +0300

    model for the mechics we have

---
+++ app/src/main/java/com/example/mobimech/models/Mechanics_here.kt
@@ -0,0 +1,7 @@
+package com.example.mobimech.models
+
+class Mechanics_here {
+        var name: String? = null
+        var specialty: String? = null
+        var description: String? = null
+    }
