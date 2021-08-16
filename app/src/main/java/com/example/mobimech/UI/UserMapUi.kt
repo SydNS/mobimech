c849ab6d59fbb7e152cb6a6bbd877341274e8f70
SydNs <sydneynsiimbe@gmail.com>
2021-08-16 07:35:29 +0300

    introduced  the use of hte bottom sheet in the app

--- app/src/main/java/com/example/mobimech/UI/UserMapUi.kt
+++ app/src/main/java/com/example/mobimech/UI/UserMapUi.kt
@@ -3,15 +3,15 @@
 package com.example.mobimech.UI
 
 import android.Manifest
+import android.app.ProgressDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
 import androidx.core.app.ActivityCompat
@@ -35,16 +35,23 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.bottomsheet_dialog.*
 import kotlin.collections.HashMap
 
 
 //Mechanic's Map Activity
 
-class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener,
     LocationListener {
     private var mMap: GoogleMap? = null
     var googleApiClient: GoogleApiClient? = null
@@ -74,12 +81,38 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
     private var txtCarName: TextView? = null
     private var profilePic: ImageView? = null
     private var relativeLayout: RelativeLayout? = null
+
+
+//    bottomsheet
+
+    lateinit var btnClose: ImageView
+    lateinit var uname: TextView
+    lateinit var phone_number: TextView
+    lateinit var save_button: ImageView
+    lateinit var close_button: ImageView
+    lateinit var profileImageView: ImageView
+    lateinit var change_picture_btn: TextView
+
+
+    private var profileChangeBtn: TextView? = null
+    private var databaseReference: DatabaseReference? = null
+    private var checker = ""
+    private var imageUri: Uri? = null
+    private var myUrl = ""
+    private var uploadTask: UploadTask? = null
+    private var storageProfilePicsRef: StorageReference? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_map_ui)
         mAuth = FirebaseAuth.getInstance()
         currentUser = mAuth!!.currentUser
-        customerID = FirebaseAuth.getInstance().getCurrentUser().uid
+
+        databaseReference = FirebaseDatabase.getInstance().reference.child("Users")
+        storageProfilePicsRef = FirebaseStorage.getInstance().reference.child("Profile Pictures")
+
+        customerID = FirebaseAuth.getInstance().currentUser?.uid
         CustomerDatabaseRef = FirebaseDatabase.getInstance().reference.child("Customer Requests")
         DriverAvailableRef = FirebaseDatabase.getInstance().reference.child("Mechanics Available")
         DriverLocationRef = FirebaseDatabase.getInstance().reference.child("Mechanics Working")
@@ -98,9 +131,63 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         SettingsButton!!.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
-            intent.putExtra("type", "Customers")
-            startActivity(intent)
+//            val intent = Intent(this, SettingsActivity::class.java)
+//            intent.putExtra("type", "Customers")
+//            startActivity(intent)
+
+            // on below line we are creating a new bottom sheet dialog.
+            val dialog = BottomSheetDialog(this)
+//
+////            // on below line we are inflating a layout file which we have created.
+            val view = layoutInflater.inflate(R.layout.bottomsheet_dialog, null)
+////
+////            // on below line we are creating a variable for our button
+//            // which we are using to dismiss our dialog.
+            btnClose = view.findViewById(R.id.close_button)
+            uname = view.findViewById(R.id.name)
+            phone_number = view.findViewById(R.id.phone_number)
+            save_button = view.findViewById(R.id.save_button)
+            close_button = view.findViewById(R.id.close_button)
+            profileImageView = view.findViewById(R.id.profile_image)
+            change_picture_btn = view.findViewById(R.id.change_picture_btn)
+////
+            change_picture_btn.setOnClickListener {
+                checker = "clicked"
+                CropImage.activity()
+                    .setAspectRatio(1, 1)
+                    .start(this)
+            }
+            save_button.setOnClickListener {
+                if (checker == "clicked") {
+                    validateControllers()
+                    dialog.dismiss()
+
+                } else {
+                    validateAndSaveOnlyInformation()
+
+                }
+
+            }
+            userInformation
+
+////            // on below line we are adding on click listener
+////            // for our dismissing the dialog button.
+            btnClose.setOnClickListener {
+////                // on below line we are calling a dismiss
+////                // method to close our dialog.
+                dialog.dismiss()
+            }
+////            // below line is use to set cancelable to avoid
+////            // closing of dialog box when clicking on the screen.
+            dialog.setCancelable(true)
+////
+////            // on below line we are setting
+////            // content view to our view.
+            dialog.setContentView(view)
+////
+////            // on below line we are calling
+////            // a show method to display a dialog.
+            dialog.show()
         }
         Logout!!.setOnClickListener {
             mAuth!!.signOut()
@@ -121,7 +208,7 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
                 }
                 driverFound = false
                 radius = 1
-                val customerId: String = FirebaseAuth.getInstance().getCurrentUser().uid
+                val customerId: String = FirebaseAuth.getInstance().currentUser!!.uid
                 val geoFire = GeoFire(CustomerDatabaseRef)
                 geoFire.removeLocation(customerId)
                 PickUpMarker?.remove()
@@ -130,16 +217,25 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
                 relativeLayout?.visibility = View.GONE
             } else {
                 requestType = true
-                val customerId: String = FirebaseAuth.getInstance().getCurrentUser().getUid()
+                val customerId: String = FirebaseAuth.getInstance().currentUser!!.uid
                 val geoFire = GeoFire(CustomerDatabaseRef)
-                geoFire.setLocation(customerId, GeoLocation(LastLocation!!.latitude, LastLocation!!.longitude))
+                geoFire.setLocation(
+                    customerId,
+                    GeoLocation(LastLocation!!.latitude, LastLocation!!.longitude)
+                )
                 CustomerPickUpLocation = LatLng(LastLocation!!.latitude, LastLocation!!.longitude)
-                PickUpMarker = mMap?.addMarker(MarkerOptions().position(CustomerPickUpLocation!!).title("My Location").icon(
-                    BitmapDescriptorFactory.fromResource(R.drawable.user)))
+                PickUpMarker = mMap?.addMarker(
+                    MarkerOptions().position(CustomerPickUpLocation!!).title("My Location").icon(
+                        BitmapDescriptorFactory.fromResource(R.drawable.user)
+                    )
+                )
                 CallCabCarButton!!.text = "Getting your Mechanic..."
                 closetDriverCab
             }
+
         }
+
+
     }//we tell driver which customer he is going to have
 
     //Show driver location on customerMapActivity
@@ -148,7 +244,13 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
     private val closetDriverCab: Unit
         get() {
             val geoFire = GeoFire(DriverAvailableRef)
-            geoQuery = geoFire.queryAtLocation(CustomerPickUpLocation?.let { GeoLocation(it.latitude, CustomerPickUpLocation!!.longitude) },
+            geoQuery = geoFire.queryAtLocation(
+                CustomerPickUpLocation?.let {
+                    GeoLocation(
+                        it.latitude,
+                        CustomerPickUpLocation!!.longitude
+                    )
+                },
                 radius.toDouble()
             )
             geoQuery?.removeAllListeners()
@@ -164,9 +266,10 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
                         //we tell driver which customer he is going to have
                         DriversRef =
                             driverFoundID?.let {
-                                FirebaseDatabase.getInstance().reference.child("Users").child("Mechanics").child(
-                                    it
-                                )
+                                FirebaseDatabase.getInstance().reference.child("Users")
+                                    .child("Mechanics").child(
+                                        it
+                                    )
                             }
                         val driversMap = HashMap<String, Any?>()
                         driversMap["CustomerRideID"] = customerID
@@ -226,7 +329,11 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
                             } else {
                                 CallCabCarButton!!.text = "Meachanic Found: $Distance"
                             }
-                            DriverMarker = mMap?.addMarker(MarkerOptions().position(DriverLatLng).title("your mechanic is here").icon(BitmapDescriptorFactory.fromResource(R.drawable.car)))
+                            DriverMarker = mMap?.addMarker(
+                                MarkerOptions().position(DriverLatLng)
+                                    .title("your mechanic is here")
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.car))
+                            )
                         }
                     }
 
@@ -239,7 +346,14 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
         mMap = googleMap
 
         // now let set user location enable
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !== PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
             return
         }
         buildGoogleApiClient()
@@ -252,8 +366,11 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
         locationRequest?.setFastestInterval(1000)
         locationRequest?.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            !== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) !== PackageManager.PERMISSION_GRANTED) {
+            !== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
             //
             return
         }
@@ -320,5 +437,132 @@ class UserMapUi : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Connec
 
                 override fun onCancelled(databaseError: DatabaseError) {}
             })
+        }
+
+
+//    userdetails
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            val result: CropImage.ActivityResult? = CropImage.getActivityResult(data)
+            if (result != null) {
+                imageUri = result.uri
+            }
+            profileImageView.setImageURI(imageUri)
+        } else {
+            startActivity(Intent(this, UserMapUi::class.java))
+
+            Toast.makeText(this, "Error, Try Again.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateControllers() {
+        if (TextUtils.isEmpty(uname.text.toString())) {
+            Toast.makeText(this, "Please provide your name.", Toast.LENGTH_SHORT).show()
+        }
+        if (TextUtils.isEmpty(phone_number.text.toString())) {
+            Toast.makeText(this, "Please provide your phone number.", Toast.LENGTH_SHORT).show()
+        }
+//        else {
+//            if (getType == "Mechanics" && TextUtils.isEmpty(driverCarName?.text.toString())) {
+//                Toast.makeText.(this, "Please provide your car Name.", Toast.LENGTH_SHORT).show()
+//            } else if (checker == "clicked") {
+        uploadProfilePicture()
+//            }
+//        }
+    }
+
+    private fun uploadProfilePicture() {
+        val progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Saving Information")
+        progressDialog.setMessage("Please wait, while we are saving your account information")
+        progressDialog.show()
+        if (imageUri != null) {
+            val fileRef: StorageReference =
+                storageProfilePicsRef!!.child((mAuth?.currentUser?.uid ?: String()) + ".jpg")
+            uploadTask = fileRef.putFile(imageUri!!)
+            uploadTask!!.continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                fileRef.downloadUrl
+            }.addOnCompleteListener { p0 ->
+                if (p0.isSuccessful) {
+                    val downloadUrl = p0.result
+                    myUrl = downloadUrl.toString()
+                    val userMap = java.util.HashMap<String, Any>()
+                    userMap["uid"] = mAuth?.currentUser!!.uid
+                    userMap["name"] = uname.text.toString()
+                    userMap["phone"] = phone_number.text.toString()
+                    userMap["image"] = myUrl
+//                    if (getType == "Mechanics") {
+//                        userMap["car"] = driverCarName?.text.toString()
+//                    }
+                    mAuth?.currentUser?.let {
+                        databaseReference?.child(it.uid)
+                            ?.updateChildren(userMap)
+                    }
+                    progressDialog.dismiss()
+
+                }
+            }
+        } else {
+            Toast.makeText(this, "Image is not selected.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateAndSaveOnlyInformation() {
+        when {
+            TextUtils.isEmpty(name?.text.toString()) -> {
+                Toast.makeText(this, "Please provide your name.", Toast.LENGTH_SHORT).show()
+            }
+            TextUtils.isEmpty(phone_number.text.toString()) -> {
+                Toast.makeText(this, "Please provide your phone number.", Toast.LENGTH_SHORT).show()
+                //        } else if (getType == "Mechanics" && TextUtils.isEmpty(driverCarName?.text.toString())) {
+                //            Toast.makeText(this, "Please provide your car Name.", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                val userMap = java.util.HashMap<String, Any>()
+                userMap["uid"] = mAuth?.currentUser!!.uid
+                userMap["name"] = uname?.text.toString()
+                userMap["phone"] = phone_number.text.toString()
+                //            if (getType == "Mechanics") {
+                //                userMap["car"] = driverCarName?.text.toString()
+                //            }
+                databaseReference?.child(mAuth?.currentUser!!.uid)?.updateChildren(userMap)
+
+            }
+        }
+    }
+
+    private val userInformation: Unit
+        get() {
+            mAuth?.currentUser?.let {
+                databaseReference?.child(it.uid)
+                    ?.addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            if (dataSnapshot.exists() && dataSnapshot.childrenCount > 0) {
+                                val name: String = dataSnapshot.child("name").value.toString()
+                                val phone: String = dataSnapshot.child("phone").value.toString()
+                                uname.text = name
+                                phone_number.text = phone
+//                                if (getType == "Drivers") {
+//                                    val car: String = dataSnapshot.child("car").value.toString()
+//                                    driverCarName?.setText(car)
+//                                }
+                                if (dataSnapshot.hasChild("image")) {
+                                    val image: String =
+                                        dataSnapshot.child("image").value.toString()
+                                    Picasso.get().load(image).into(profileImageView)
+                                }
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {}
+                    })
+            }
         }
 }
